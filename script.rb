5b14351a4ed326f425a4295e164f1d11b251847e
MclPio <113801014+MclPio@users.noreply.github.com>
2023-01-13 18:20:18 -0500

    set up function and dictionary variable with instructions

---
+++ script.rb
@@ -0,0 +1,14 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(word, dictionary)
+
+end
+
+substrings("below", dictionary)
+
+=begin
+  > dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+  => ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+  > substrings("below", dictionary)
+  => { "below" => 1, "low" => 1 }
+=end
